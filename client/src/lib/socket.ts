b15dbacf6aa4473851587e2f81f4d9a2e701d991
Lalit Max <lalitmay@gmail.com>
2024-10-11 16:16:16 +0530

    add socket backend live server

--- client/src/lib/socket.ts
+++ client/src/lib/socket.ts
@@ -1,7 +1,5 @@
 // lib/socket.ts
 import { io, Socket } from 'socket.io-client';
-
-const socket: Socket = io('http://localhost:3005');
-// const socket: Socket = io('https://group-chat-backend-wqeh.onrender.com');
+const socket: Socket = io('https://realtime-code-editor-with-video.onrender.com');
 
 export default socket;
